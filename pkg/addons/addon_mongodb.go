c683e69f35ea20fec4145c65b747dbaa03292903
Jan <mail@jezekjan.com>
2018-05-19 11:54:52 +0200

    first attempt to implement mongodb

---
+++ pkg/addons/addon_mongodb.go
@@ -0,0 +1,81 @@
+package addons
+
+import (
+	"fmt"
+	"github.com/xetys/hetzner-kube/pkg/clustermanager"
+)
+
+//MongoDbAddon installs MongoDb as ReplicaSet
+type MongoDbAddon struct {
+	masterNode   *clustermanager.Node
+	communicator clustermanager.NodeCommunicator
+}
+
+//NewMongoDbAddon creates an addon which installs MongoDb
+func NewMongoDbAddon(provider clustermanager.ClusterProvider, communicator clustermanager.NodeCommunicator) ClusterAddon {
+	masterNode, _ := provider.GetMasterNode()
+	return &MongoDbAddon{masterNode: masterNode, communicator: communicator}
+}
+
+func init() {
+	addAddon(NewMongoDbAddon)
+}
+
+//Name returns the addons name
+func (addon MongoDbAddon) Name() string {
+	return "mongodb"
+}
+
+//Requires returns a slice with the name of required addons
+func (addon MongoDbAddon) Requires() []string {
+	return []string{}
+}
+
+//Description returns the addons description
+func (addon MongoDbAddon) Description() string {
+	return "Simple scalable block storage provider"
+}
+
+//URL returns the URL of the addons underlying project
+func (addon MongoDbAddon) URL() string {
+	return "https://www.mongodb.com/"
+}
+
+//Install performs all steps to install the addon
+func (addon MongoDbAddon) Install(args ...string) {
+	node := *addon.masterNode
+
+	_, err := addon.communicator.RunCmd(node, "kubectl apply -f https://raw.githubusercontent.com/openebs/openebs/master/k8s/demo/mongodb/mongo-statefulset.yml")
+	FatalOnError(err)
+
+/*
+	improvedStorageClass := `apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+   name: openebs-standard
+provisioner: openebs.io/provisioner-iscsi
+parameters:
+  openebs.io/storage-pool: "default"
+  openebs.io/jiva-replica-count: "3"
+  openebs.io/volume-monitor: "true"
+  openebs.io/capacity: 5G`
+	err = addon.communicator.WriteFile(node, "/root/openebs-storageclass.yaml", improvedStorageClass, false)
+	FatalOnError(err)
+
+	_, err = addon.communicator.RunCmd(node, "kubectl delete -f openebs-storageclass.yaml && kubectl apply -f openebs-storageclass.yaml")
+	FatalOnError(err)
+
+*/
+
+	fmt.Println("MongoDb installed")
+}
+
+//Uninstall performs all steps to remove the addon
+func (addon MongoDbAddon) Uninstall() {
+	node := *addon.masterNode
+
+	_, err := addon.communicator.RunCmd(node, "kubectl delete -f https://raw.githubusercontent.com/openebs/openebs/master/k8s/demo/mongodb/mongo-statefulset.yml")
+	FatalOnError(err)
+
+	fmt.Println("MongoDb removed")
+}
